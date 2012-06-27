14bab16c6a682c52ec4de9ac0808829d7b33e0f6
Julien Lirochon <julien@lirochon.net>
2012-06-27 13:58:18 +0200

    added PyPI package files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='django-simple-rbac',
+    version='0.1.0',
+    author='SportEasy',
+    author_email='contact@sporteasy.fr',
+    packages=['django_simple_rbac'],
+    url='https://github.com/sporteasy/django-simple-rbac',
+    license='LICENSE.txt',
+    description='Simple RBAC for Django 1.4.x.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Django >= 1.4.0",
+        "simple-rbac >= 0.1.1",
+    ],
+)
