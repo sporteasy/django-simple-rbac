a44b5fa8747342fef552c362258bd0a67adf7e39
shostelet <simon.hostelet@gmail.com>
2017-08-25 14:35:40 +0200

    decorator require_privilege 'resource' argument can now be a callable taking request as argument. It allow for example to write such code: my_view = require_privilege('edit', lambda request: request.user)(EditUserView.as_view())
    Therefore simplifying get or post method of the View if it is needed to check authorisation against a object rather than just a resource name.

--- django_simple_rbac/middleware.py
+++ django_simple_rbac/middleware.py
@@ -22,7 +22,11 @@ class ACLMiddleware(object):
 
             # check privilege, return None if allowed
             privilege = view_func.required_privilege
-            granted = is_allowed(request, privilege['operation'], privilege['resource'])
+            if callable(privilege['resource']):
+                resource = privilege['resource'](request)
+            else:
+                resource = privilege['resource']
+            granted = is_allowed(request, privilege['operation'], resource)
             if granted:
                 return None
 
@@ -37,7 +41,7 @@ class ACLMiddleware(object):
 
             # Finally deny access. We have to return a response here because any raised exception wouldn't be caught
             # by process_exception method (it catches exceptions only when they are raised by a view)
-            return self._create_403_response(request, privilege['operation'], privilege['resource'],
+            return self._create_403_response(request, privilege['operation'], resource,
                                              authority=granted.authority, template_name=template_name)
 
     def process_exception(self, request, exception):
